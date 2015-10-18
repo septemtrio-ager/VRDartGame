15941797321f43ea6bf26c992c7bb0fd7a63c5dc
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-10-18 23:56:14 +0900

    マスク用ウィンドウを使わないようにした

--- ARSwithWML/Sample4_AR/arstest.cpp
+++ ARSwithWML/Sample4_AR/arstest.cpp
@@ -72,15 +72,15 @@ UINT MainLoop(WindowManager *winmgr)
 	
 	
 	// mask windowに表示されるオブジェクト
-	Window maskWindow;
-	winmgr->RegisterWindow(&maskWindow);
+	// Window maskWindow;
+	// winmgr->RegisterWindow(&maskWindow);
 	
-	ARSG maskG(maskWindow.hWnd, sizex, sizey, true);
-	maskG.SetBackgroundColor(255, 0, 0, 0);
+	// ARSG maskG(maskWindow.hWnd, sizex, sizey, true);
+	// maskG.SetBackgroundColor(255, 0, 0, 0);
 	
-	Light maskLight(&maskG);
-	maskLight.SetLightIntensity(5.0f);
-	maskG.Register(&maskLight);
+	// Light maskLight(&maskG);
+	// maskLight.SetLightIntensity(5.0f);
+	// maskG.Register(&maskLight);
 	
 	// ウィンドウに表示される映像
 	Texture mainScreen(&g, sizex, sizey);
@@ -88,9 +88,9 @@ UINT MainLoop(WindowManager *winmgr)
 	g.Register(&mainScreen);
 
 	// mask
-	Texture maskScreen(&maskG, sizex, sizey);
-	maskScreen.SetDrawMode(TRUE);
-	maskG.Register(&maskScreen);
+	// Texture maskScreen(&maskG, sizex, sizey);
+	// maskScreen.SetDrawMode(TRUE);
+	// maskG.Register(&maskScreen);
 
 	
 
@@ -103,26 +103,25 @@ UINT MainLoop(WindowManager *winmgr)
 	// 手とダーツが接触している領域を格納する
 	Texture hitArea_Hand_and_Dart(&g,sizex,sizey);
 	// ダーツの矢とダーツ台が接触している領域を格納のする
-	Texture hitArea_Dart_and_DartBoard0(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard0(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard0.SetDrawMode(TRUE);
-	Texture hitArea_Dart_and_DartBoard1(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard1(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard1.SetDrawMode(TRUE);
-	Texture hitArea_Dart_and_DartBoard2(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard2(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard2.SetDrawMode(TRUE);
-	Texture hitArea_Dart_and_DartBoard3(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard3(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard3.SetDrawMode(TRUE);
-	maskG.Register(&hitArea_Dart_and_DartBoard3);
-	Texture hitArea_Dart_and_DartBoard4(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard4(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard4.SetDrawMode(TRUE);
-	Texture hitArea_Dart_and_DartBoard5(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard5(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard5.SetDrawMode(TRUE);
-	Texture hitArea_Dart_and_DartBoard6(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard6(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard6.SetDrawMode(TRUE);
-	Texture hitArea_Dart_and_DartBoard7(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard7(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard7.SetDrawMode(TRUE);
-	Texture hitArea_Dart_and_DartBoard8(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard8(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard8.SetDrawMode(TRUE);
-	Texture hitArea_Dart_and_DartBoard9(&maskG, sizex, sizey);
+	Texture hitArea_Dart_and_DartBoard9(&g, sizex, sizey);
 	hitArea_Dart_and_DartBoard9.SetDrawMode(TRUE);
 	
 	// 流れてくるフレームを一時的に保存する
@@ -132,7 +131,7 @@ UINT MainLoop(WindowManager *winmgr)
 	source.SetDrawMode(TRUE);
 
 	// mask
-	Texture maskSource(&maskG, sizex, sizey);
+	Texture maskSource(&g, sizex, sizey);
 	maskSource.SetDrawMode(TRUE);
 	
 	// ダーツの矢
@@ -149,40 +148,40 @@ UINT MainLoop(WindowManager *winmgr)
 
 	// mask
 	// マスクとなるダーツ
-	Dart maskDart(&maskG, L"../../../material/white_dart.x");
+	Dart maskDart(&g, L"../../../material/white_dart.x");
 	maskDart.SetScale(2.0f, 2.0f, 2.0f);
 	maskDart.SetPosition(6.0f, 3.0f, 0.0f);
-	// maskG.Register(&maskDart);
+	// g.Register(&maskDart);
 		
 	// ダーツ台の得点の範囲のマスク
-	DartBoard dartBoardmask00(&maskG, L"../../../material/mask/0mask.x");
+	DartBoard dartBoardmask00(&g, L"../../../material/mask/0mask.x");
 	dartBoardmask00.SetScale(0.8f, 0.8f,0.8f);
 	dartBoardmask00.SetPosition(-6.5f, 0.0f, 0.0f);
-	DartBoard dartBoardmask01(&maskG, L"../../../material/mask/1mask.x");
+	DartBoard dartBoardmask01(&g, L"../../../material/mask/1mask.x");
 	dartBoardmask01.SetScale(0.8f, 0.8f, 0.8f);
 	dartBoardmask01.SetPosition(-6.5f, 0.0f, 0.0f);
-	DartBoard dartBoardmask02(&maskG, L"../../../material/mask/2mask.x");
+	DartBoard dartBoardmask02(&g, L"../../../material/mask/2mask.x");
 	dartBoardmask02.SetScale(0.8f, 0.8f, 0.8f);
 	dartBoardmask02.SetPosition(-6.5f, 0.0f, 0.0f);
-	DartBoard dartBoardmask03(&maskG, L"../../../material/mask/3mask.x");
+	DartBoard dartBoardmask03(&g, L"../../../material/mask/3mask.x");
 	dartBoardmask03.SetScale(0.8f, 0.8f, 0.8f);
 	dartBoardmask03.SetPosition(-6.5f, 0.0f, 0.0f);
-	DartBoard dartBoardmask04(&maskG, L"../../../material/mask/4mask.x");
+	DartBoard dartBoardmask04(&g, L"../../../material/mask/4mask.x");
 	dartBoardmask04.SetScale(0.8f, 0.8f, 0.8f);
 	dartBoardmask04.SetPosition(-6.5f, 0.0f, 0.0f);
-	DartBoard dartBoardmask05(&maskG, L"../../../material/mask/5mask.x");
+	DartBoard dartBoardmask05(&g, L"../../../material/mask/5mask.x");
 	dartBoardmask05.SetScale(0.8f, 0.8f, 0.8f);
 	dartBoardmask05.SetPosition(-6.5f, 0.0f, 0.0f);
-	DartBoard dartBoardmask06(&maskG, L"../../../material/mask/6mask.x");
+	DartBoard dartBoardmask06(&g, L"../../../material/mask/6mask.x");
 	dartBoardmask06.SetScale(0.8f, 0.8f, 0.8f);
 	dartBoardmask06.SetPosition(-6.5f, 0.0f, 0.0f);
-	DartBoard dartBoardmask07(&maskG, L"../../../material/mask/7mask.x");
+	DartBoard dartBoardmask07(&g, L"../../../material/mask/7mask.x");
 	dartBoardmask07.SetScale(0.8f, 0.8f, 0.8f);
 	dartBoardmask07.SetPosition(-6.5f, 0.0f, 0.0f);
-	DartBoard dartBoardmask08(&maskG, L"../../../material/mask/8mask.x");
+	DartBoard dartBoardmask08(&g, L"../../../material/mask/8mask.x");
 	dartBoardmask08.SetScale(0.8f, 0.8f, 0.8f);
 	dartBoardmask08.SetPosition(-6.5f, 0.0f, 0.0f);
-	DartBoard dartBoardmask09(&maskG, L"../../../material/mask/9mask.x");
+	DartBoard dartBoardmask09(&g, L"../../../material/mask/9mask.x");
 	dartBoardmask09.SetScale(0.8f, 0.8f, 0.8f);
 	dartBoardmask09.SetPosition(-6.5f, 0.0f, 0.0f);
 		
@@ -225,19 +224,19 @@ UINT MainLoop(WindowManager *winmgr)
 			
 		// ダーツの矢と手が接触している領域のマスク画像を作成
 		subtract_maskf(&hitArea_Hand_and_Dart,&stored,&source,0x20202020);
-		// subtract_maskf(&hitArea_Dart_and_DartBoard0, &maskG, &maskG, 0x20202020);
+		// subtract_maskf(&hitArea_Dart_and_DartBoard0, &g, &g, 0x20202020);
 
 		// ダーツの矢とダーツ台が接触している領域のマスク画像を作成
-		maskG.Draw(&dartBoardmask00, &hitArea_Dart_and_DartBoard0);
-		maskG.Draw(&dartBoardmask01, &hitArea_Dart_and_DartBoard1);
-		maskG.Draw(&dartBoardmask02, &hitArea_Dart_and_DartBoard2);
-		maskG.Draw(&dartBoardmask03, &hitArea_Dart_and_DartBoard3);
-		maskG.Draw(&dartBoardmask04, &hitArea_Dart_and_DartBoard4);
-		maskG.Draw(&dartBoardmask05, &hitArea_Dart_and_DartBoard5);
-		maskG.Draw(&dartBoardmask06, &hitArea_Dart_and_DartBoard6);
-		maskG.Draw(&dartBoardmask07, &hitArea_Dart_and_DartBoard7);
-		maskG.Draw(&dartBoardmask08, &hitArea_Dart_and_DartBoard8);
-		maskG.Draw(&dartBoardmask09, &hitArea_Dart_and_DartBoard9);
+		g.Draw(&dartBoardmask00, &hitArea_Dart_and_DartBoard0);
+		g.Draw(&dartBoardmask01, &hitArea_Dart_and_DartBoard1);
+		g.Draw(&dartBoardmask02, &hitArea_Dart_and_DartBoard2);
+		g.Draw(&dartBoardmask03, &hitArea_Dart_and_DartBoard3);
+		g.Draw(&dartBoardmask04, &hitArea_Dart_and_DartBoard4);
+		g.Draw(&dartBoardmask05, &hitArea_Dart_and_DartBoard5);
+		g.Draw(&dartBoardmask06, &hitArea_Dart_and_DartBoard6);
+		g.Draw(&dartBoardmask07, &hitArea_Dart_and_DartBoard7);
+		g.Draw(&dartBoardmask08, &hitArea_Dart_and_DartBoard8);
+		g.Draw(&dartBoardmask09, &hitArea_Dart_and_DartBoard9);
 		
 		// ダーツ台を回転させる
 		dartBoard.SetRotationX(0.05f);
@@ -345,7 +344,7 @@ UINT MainLoop(WindowManager *winmgr)
 		//arsgd.Draw(&debug);
 		
 		g.Draw();
-		maskG.Draw();
+		// maskG.Draw();
 	}
 	
 	d.StopGraph();
